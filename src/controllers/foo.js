bffae56dea66ff838cc4c1767d5d8940b746f58f
Gabriel Charleboix <gcharleboix@gmail.com>
2016-12-13 23:52:29 -0800

    slightly better

--- src/controllers/foo.js
+++ src/controllers/foo.js
@@ -1,8 +1,12 @@
-export default class FooController {  
+export default class FooController {
   constructor($scope) {
+    this.scope = $scope;
     $scope.foo = "foo";
-    $scope.toggle = () => {
-      $scope.foo = $scope.foo === "foo" ? "bar" : "foo";
-    };
-  };
-}
+    $scope.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    const foo = this.scope.foo;
+    this.scope.foo = foo === "foo" ? "bar" : "foo";
+  }
+};
